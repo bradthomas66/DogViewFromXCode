0a9508067d09ba009693ced0e476a382464cd74f
Brad Thomas <bradley-thomas1@hotmail.com>
2020-07-01 18:55:24 -0600

    Initial Commit

---
+++ DogView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DogView
+//
+//  Created by Brad Thomas on 2020-07-01.
+//  Copyright © 2020 Brad Thomas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
